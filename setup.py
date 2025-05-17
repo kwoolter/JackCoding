fa4c9fda6d9749702a22e5d783f31397d4f08098
keith <keith.woolterton@gmail.com>
2025-05-17 09:18:34 +0100

    Tidy up

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='JackCoding',
+    version='1.0.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='keith',
+    author_email='',
+    description='',
+    install_requires=['colorama', 'pandas']
+)
